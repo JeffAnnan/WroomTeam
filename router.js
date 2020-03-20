858e44433c7e02f6ef8845ec77e956cc8cb884c8
laville16 <49551892+laville16@users.noreply.github.com>
2020-03-20 14:29:09 +0100

    Add files via upload
    ajouter pilote

---
+++ router.js
@@ -0,0 +1,42 @@
+
+let HomeController = require('./../controllers/HomeController');
+let ResultatController = require('./../controllers/ResultatController');
+let EcurieController = require('./../controllers/EcurieController');
+let PiloteController = require('./../controllers/PiloteController');
+let CircuitController = require('./../controllers/CircuitController');
+
+// Routes
+module.exports = function(app){
+
+// Main Routes
+    app.get('/', HomeController.Index);
+    app.get('/menu', HomeController.Index);
+
+// accueil
+app.post('/menu', HomeController.Connexion);
+    app.post('/', HomeController.Connexion);
+
+// pilotes
+    app.get('/pilotes', PiloteController.Pilotes);
+    app.get('/ajout', PiloteController.AjoutPilote);
+    app.post('/piloteAjoute', PiloteController.FinAjoutPilote);
+    //app.get('/detailInfoPilote/:pilnum', PiloteController.DetailInfoPilote);
+
+ // circuits
+   app.get('/circuits', CircuitController.ListerCircuit);
+   app.get('/detailCircuit/:cirnum', CircuitController.DetailCircuit);
+
+// Ecuries
+   app.get('/ecuries', EcurieController.ListerEcurie);
+
+ //Résultats
+   app.get('/resultats', ResultatController.ListerResultat);
+   app.get('/GrandPrix/:gpnum', ResultatController.DetailResultat);
+
+
+
+// tout le reste
+app.get('*', HomeController.NotFound);
+app.post('*', HomeController.NotFound);
+
+};
